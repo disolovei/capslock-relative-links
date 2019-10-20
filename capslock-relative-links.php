0d8f910065ee904a4d695fdd9b3e27698a1fb5a1
Dima Solovey <vlnsk@ukr.net>
2019-10-20 20:48:42 +0300

    Add a plugin files

---
+++ capslock-relative-links.php
@@ -0,0 +1,48 @@
+<?php
+/*
+ * Plugin Name: CapsLock Relative links
+ * Description: Description
+ * Author: Dima Solovey
+ * Version: 1.0 
+ */
+
+defined( 'ABSPATH' )|| exit;
+
+if ( ! defined( 'CPLCK_SITE_URL' ) ) {
+    define( 'CPLCK_SITE_URL', site_url() );
+}
+
+if ( ! defined( 'CPLCK_HOME_URL' ) ) {
+    define( 'CPLCK_HOME_URL', home_url() );
+}
+
+/**
+ * 
+ */
+function cplck_replacer( $link, $is_home = true ) {
+    return '/' . trim( str_replace( ( $is_home ? CPLCK_HOME_URL : CPLCK_SITE_URL ), '', $link ), '/' );
+}
+
+/**
+ * 
+ */
+function cplck_replacer_site_url( $link ) {
+    return cplck_replacer( $link, false );
+}
+
+/**
+ * 
+ */
+function cplck_replacer_home_url( $link ) {
+    return cplck_replacer( $link );
+}
+
+if ( ! is_admin() ) {
+    add_filter( 'page_link', 'cplck_replacer_home_url' );
+    add_filter( 'attachment_link', 'cplck_replacer_home_url' );
+    add_filter( 'post_type_link', 'cplck_replacer_home_url' );
+    add_filter( 'term_link', 'cplck_replacer_home_url' );
+    add_filter( 'wp_get_attachment_url', 'cplck_replacer_site_url' );
+    add_filter( 'theme_root_uri', 'cplck_replacer_site_url' );
+    add_filter( 'plugins_url', 'cplck_replacer_site_url' );
+}
